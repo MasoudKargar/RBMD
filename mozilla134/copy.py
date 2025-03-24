cd574a9cc989ca74db6e144af358a582e6f6d399
Masoud Kargar <masoud.karegar@gmail.com>
2025-03-25 02:00:52 +0330

    Add files via upload

---
+++ mozilla134/copy.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+target_folder = 'target'
+
+folders_to_create = ['accessible', 'browser', 'build', 'caps', 'docshell', 'dom', 'extensions', 'gfx', 'intl', 'ipc']
+
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
+
+for folder in folders_to_create:
+    folder_path = os.path.join(target_folder, folder)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+# source_folder = 'gecko-dev-master' Download from https://github.com/mozilla/gecko-dev/tree/master
+
+def copy_files(src_folder, dst_folder, extensions):
+    for root, dirs, files in os.walk(src_folder):
+        for file in files:
+            if file.endswith(extensions):
+                src_file = os.path.join(root, file)
+                dst_file = os.path.join(dst_folder, file)
+                shutil.copy(src_file, dst_file)
+
+for folder in folders_to_create:
+    source_subfolder = os.path.join(source_folder, folder)
+    target_subfolder = os.path.join(target_folder, folder)
+    copy_files(source_subfolder, target_subfolder, ('.cpp', '.h'))
+
+print("All files were copied successfully.")
